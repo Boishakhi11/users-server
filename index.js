c57961870adaaa84bc1ed96db11d0ca8fa9e9118
Boishakhi G <bgmukta11@gmail.com>
2026-03-05 23:32:02 +0100

    data is recieving from client

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(express.json());
+
+const users = [
+  { id: 1, name: "mukta", email: "mukta10@gmail.com" },
+  { id: 2, name: "mukti", email: "mukti@gmail.com" },
+  { id: 3, name: "mala", email: "mukt@gmail.com" },
+];
+
 app.get("/", (req, res) => {
   res.send("Server is running");
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log("post method called", req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
 });
 
 app.listen(port, () => {
